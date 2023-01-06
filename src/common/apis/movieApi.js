b126aeaee23bb27a1068fdf92c528ff77bb79eb0
Gokul Gandhi <gokulgandhi97@gmail.com>
2023-01-06 20:28:42 +0530

    updated baseURL

--- src/common/apis/movieApi.js
+++ src/common/apis/movieApi.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // initially we were creating a base url and exporting them
 export default axios.create({
-  baseURL: "http://www.omdbapi.com",
+  baseURL: "https://www.omdbapi.com",
 });
